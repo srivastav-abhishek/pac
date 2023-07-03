b4abf294678668de245c7dda958697c2eda51c45
Karthik K N <karthikkn1997@gmail.com>
2023-07-03 14:12:09 +0530

    Fetch quota associated with each group

--- internal/pkg/pac-go-server/models/group.go
+++ internal/pkg/pac-go-server/models/group.go
@@ -4,5 +4,6 @@ type Group struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 	// Membership is a flag to indicate if the user is a member of the group
-	Membership bool `json:"membership"`
+	Membership bool     `json:"membership"`
+	Quota      Capacity `json:"quota"`
 }
